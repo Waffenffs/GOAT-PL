eed230efd25491f518568a5088aa539598276b33
WAFFEN AMPATUA <league.blades@gmail.com>
2023-02-11 17:47:42 +0800

    first commit

---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDVGHsLs4G4NauzHOqu9aiD9MRhrUMOaFk",
+  authDomain: "todo-app-5bf04.firebaseapp.com",
+  projectId: "todo-app-5bf04",
+  storageBucket: "todo-app-5bf04.appspot.com",
+  messagingSenderId: "986248650805",
+  appId: "1:986248650805:web:fc1c9dfb32dcc8ccb5517c",
+  measurementId: "G-XK2B8FQ749"
+};
